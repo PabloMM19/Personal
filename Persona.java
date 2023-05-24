c53f99e24a4bde0b7c1b5a9e8d71822e1e117c52
Pablo <pablo@mail.com>
2023-05-24 22:53:47 +0200

    Añadido correo

--- Persona.java
+++ Persona.java
@@ -1,9 +1,11 @@
 public class Persona {
 private String nombre;
 private int edad;
-public Persona(String nombre, int edad){
+private String correo;
+public Persona(String nombre, int edad, String correo){
 this.nombre = nombre;
 this.edad = edad;
+this.correo = correo;
 }
 //getters and setters
 public String getNombre(){
@@ -16,4 +18,5 @@ return this.edad;
 public void imprimirDatos() {
 System.out.println("Nombre: " + nombre);
 System.out.println("Edad: " + edad);
+System.out.println("Correo: " + correo);
 }
